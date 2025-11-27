e6e1f471f76a0ef416b18dc3dbb54072190fce19
curiouscalala <sarahdahner97@gmail.com>
2025-11-27 15:55:27 +0800

    test 1

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -9,7 +9,7 @@ test("App form submission", async () => {
     const emailInput = screen.getByTestId("emailInput");
     const submitButton = screen.getByTestId("submitButton");
 
-    await userEvent.type(emailInput, "sarah@sarah.com");
+    await userEvent.type(emailInput, "sarah");
 
     expect(submitButton.disabled).toBe(false);
 }) 
